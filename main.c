3c7e980e6e472cd7e4fe91520324a3db62a5d700
waax71 <axel.wachter@powersurf.li>
2022-10-06 14:05:17 +0200

    a message

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "myLib.h"
+
+int main()
+{
+    printf("Hello world! This is myApp using myLib\n");
+    showVersion();
+    showTime();
+    return 0;
+}
